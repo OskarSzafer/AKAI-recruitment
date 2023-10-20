a9b2f548662b99bd7d15c7190985034296e8df7c
OskarSzafer <68118272+OskarSzafer@users.noreply.github.com>
2023-10-20 20:27:19 +0200

    word-rank finish

---
+++ word-rank.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+
+# input: array with multiple strings
+# expected output: rank of the 3 most often repeated words in given set of strings and number of times they occured, case insensitive
+
+sentences = [
+    'Taki mamy klimat',
+    'Wszędzie dobrze ale w domu najlepiej',
+    'Wyskoczył jak Filip z konopii',
+    'Gdzie kucharek sześć tam nie ma co jeść',
+    'Nie ma to jak w domu',
+    'Konduktorze łaskawy zabierz nas do Warszawy',
+    'Jeżeli nie zjesz obiadu to nie dostaniesz deseru',
+    'Bez pracy nie ma kołaczy',
+    'Kto sieje wiatr ten zbiera burzę',
+    'Być szybkim jak wiatr',
+    'Kopać pod kimś dołki',
+    'Gdzie raki zimują',
+    'Gdzie pieprz rośnie',
+    'Swoją drogą to gdzie rośnie pieprz?',
+    'Mam nadzieję, że poradzisz sobie z tym zadaniem bez problemu',
+    'Nie powinno sprawić żadnego problemu, bo Google jest dozwolony',
+]
+
+# Example result:
+# 1. "mam" - 12
+# 2. "tak" - 5
+# 3. "z" - 2
+
+encountered = []
+number = []
+
+for sentence in sentences:
+    sentence = sentence.split()
+    for word in sentence:
+        word = word.lower()
+        if word in encountered:
+            number[encountered.index(word)] += 1
+        else:
+            encountered.append(word)
+            number.append(1)
+
+for i in range(len(encountered)):
+    print(encountered[i], number[i])
+
+for i in range(1, 4):
+    print(f"{i}. \"{encountered[number.index(max(number))]}\" - {max(number)}")
+    number[number.index(max(number))] = 0
