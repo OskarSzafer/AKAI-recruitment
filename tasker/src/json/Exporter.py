08576f193c43844ba6e9019ae440513894b854d2
OskarSzafer <68118272+OskarSzafer@users.noreply.github.com>
2023-10-21 01:07:23 +0200

    indent correct

--- tasker/src/json/Exporter.py
+++ tasker/src/json/Exporter.py
@@ -7,7 +7,7 @@ class Exporter:
     #    pass
 
     def save_tasks(self, tasks):
-        self.f = json.dumps(tasks, indent=4)
+        self.f = json.dumps(tasks, indent=len(tasks))
  
         with open("taski.json", "w") as file:
             file.write(self.f)
